836be24f953a7430cd4415a197faa6a1fcbee3dd
arieljlira@gmail.com <arieljlira@gmail.com@6d4fc312-373e-aad0-5269-f45233a7996e>
2012-02-25 21:58:18 +0000

    Agrego Auth basica

--- trunk/src/Cpm/JovenesBundle/Controller/DefaultController.php
+++ trunk/src/Cpm/JovenesBundle/Controller/DefaultController.php
@@ -2,19 +2,75 @@
 
 namespace Cpm\JovenesBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+use JMS\SecurityExtraBundle\Annotation\Secure;
 
 class DefaultController extends Controller
 {
     
     /**
-     * @Route("/hello/{name}", name="_demo_hello")
+     * @Route("/", name="home")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
+    {
+        return array('name' => 'index');
+    }
+    
+    
+    /**
+     * @Route("/login", name="_login")
+     * @Template()
+     */
+    public function loginAction()
+    {
+        if ($this->get('request')->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $this->get('request')->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $this->get('request')->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return array(
+            'last_username' => $this->get('request')->getSession()->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error,
+        );
+    }
+
+    /**
+     * @Route("/login_check", name="_security_check")
+     */
+    public function securityCheckAction()
+    {
+        // The security layer will intercept this request
+    }
+
+    /**
+     * @Route("/logout", name="_logout")
+     */
+    public function logoutAction()
+    {
+        // The security layer will intercept this request
+    }
+
+    /**
+     * @Route("/hello", defaults={"name"="World"}),
+     * @Route("/hello/{name}", name="_secured_hello")
+     * @Template()
+     */
+    public function helloAction($name)
+    {
+        return array('name' => $name);
+    }
+
+    /**
+     * @Route("/hello/admin/{name}", name="_secured_hello_admin")
+     * @Secure(roles="ROLE_ADMIN")
+     * @Template()
+     */
+    public function helloadminAction($name)
     {
         return array('name' => $name);
     }
