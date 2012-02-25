4217191fc884fbb0762e54af6e09fafcecab6702
arieljlira@gmail.com <arieljlira@gmail.com@6d4fc312-373e-aad0-5269-f45233a7996e>
2012-02-25 13:04:14 +0000

    Cambio el routing por annotations

--- trunk/src/Cpm/JovenesBundle/Controller/DefaultController.php
+++ trunk/src/Cpm/JovenesBundle/Controller/DefaultController.php
@@ -4,12 +4,18 @@ namespace Cpm\JovenesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
     
+    /**
+     * @Route("/hello/{name}", name="_demo_hello")
+     * @Template()
+     */
     public function indexAction($name)
     {
-        return $this->render('CpmJovenesBundle:Default:index.html.twig', array('name' => $name));
+        return array('name' => $name);
     }
 }
