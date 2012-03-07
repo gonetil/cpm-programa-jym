53ab3f265a5ee414f10db110951a3d0f907d9721
gonetil <gonetil@gmail.com>
2012-03-07 23:18:52 +0000

    se asigna una clave vacia al crear colaboradores

--- src/Cpm/JovenesBundle/Controller/PerfilController.php
+++ src/Cpm/JovenesBundle/Controller/PerfilController.php
@@ -96,9 +96,12 @@ class PerfilController extends BaseController
     	$colaboradores = $proyecto->getColaboradores();
     	foreach ($colaboradores as $colaborador) {
     		if ($c = $this->getRepository('CpmJovenesBundle:Usuario')->findOneByEmail($colaborador->getEmail())) //el colaborador ya existia en la bbdd 
-    		{ 
+    		{ //si el email del colaborador estaba en la BBDD, no creo uno nuevo  
     			$colaboradores->removeElement($colaborador);
     			$colaboradores->add($c);
+    		} else 
+    		{ //si el colaborador debe ser cargado en la BBDD, le pongo una password vacia
+    			$colaborador->setPassword("");
     		}
     	}
     	 
