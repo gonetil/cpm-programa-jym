7d25af26e67a6d4c1f3bcdf918abe684911ad3ef
gonetil <gonetil@gmail.com>
2012-04-05 13:44:51 +0000

    en el wizzard, cambio los ENTER por TAB para evitar submit accidentales

--- web/js/wizzard_form.js
+++ web/js/wizzard_form.js
@@ -55,13 +55,32 @@
    		}
    }
    
-   
+   //al presionar ENTER se genera un TAB
+   function enter_to_tab(){
+	  
+	   $("input").not( $(":button") ).keypress(function (evt) {
+		   if (evt.keyCode == 13) {
+			   iname = $(this).val();
+			   if (iname !== 'Submit'){
+				   var fields = $(this).parents('form:eq(0),body').find('button, input, textarea, select');
+				   var index = fields.index( this );
+				   if ( index > -1 && ( index + 1 ) < fields.length ) {
+					   	fields.eq( index + 1 ).focus();
+				   }
+			
+				   return false;
+			   }
+		   }
+	   });
+   }
+
    
    
    $(document).ready(function() {
    					
 					setup_validation();
-					setup_selects(); 							   				
+					setup_selects();
+					enter_to_tab();
 		   			move_stage(0,1,false); 
 		   			
 					
