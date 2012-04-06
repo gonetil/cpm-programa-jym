39dc94a4885ed3021a28ec1363b479ba04f3f855
arieljlira@gmail.com <arieljlira@gmail.com@6d4fc312-373e-aad0-5269-f45233a7996e>
2012-04-06 16:01:11 +0000

    prueba para ver si se soluciona en parte el problema de date_format

--- trunk/web/js/utils.js
+++ trunk/web/js/utils.js
@@ -137,10 +137,27 @@ function definirIntervalo(inicioID, finID){
 	});
 }
 $(function() {
-	$.datepicker.setDefaults( $.datepicker.regional[ "es" ] );
-	$.datepicker.setDefaults( { dateFormat: 'dd/mm/yy' } );
-	$.datepicker.setDefaults( { monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'] });
-	$.datepicker.setDefaults( { monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'] });
+	jQuery(function($){
+		$.datepicker.regional['es'] = {
+			closeText: 'Cerrar',
+			prevText: '&#x3c;Ant',
+			nextText: 'Sig&#x3e;',
+			currentText: 'Hoy',
+			monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio',
+			'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+			monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun',
+			'Jul','Ago','Sep','Oct','Nov','Dic'],
+			dayNames: ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado'],
+			dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
+			dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+			weekHeader: 'Sm',
+			dateFormat: 'dd/mm/yy',
+			firstDay: 1,
+			isRTL: false,
+			showMonthAfterYear: false,
+			yearSuffix: ''};
+		$.datepicker.setDefaults($.datepicker.regional['es']);
+	});
 	
 	$('.datepicker').datepicker();
 });
