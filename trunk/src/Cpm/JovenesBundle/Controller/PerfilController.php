801020e4dd3ff5f777eba03244b084aced4635d0
gonetil@gmail.com <gonetil@gmail.com@6d4fc312-373e-aad0-5269-f45233a7996e>
2012-04-16 23:10:10 +0000

    agrego las extensiones que mandó juan

--- trunk/src/Cpm/JovenesBundle/Controller/PerfilController.php
+++ trunk/src/Cpm/JovenesBundle/Controller/PerfilController.php
@@ -410,7 +410,7 @@ class PerfilController extends BaseController
     }
    
     private function getValidExtensions() {
-    	return $valid_extensions = array("doc","docx","odt","pdf");
+    	return $valid_extensions = array("doc","docx","odt","pdf","rtf","wps");
     }
     	
     /**
