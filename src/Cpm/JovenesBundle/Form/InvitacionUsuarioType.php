25a70fdde45f3fd6a2f24762061f57ad05451804
gonetil <gonetil@gmail.com>
2012-04-18 10:35:38 +0000

    agrego tipo de escuela al excel cambio label del suplente en invitacion

--- src/Cpm/JovenesBundle/Form/InvitacionUsuarioType.php
+++ src/Cpm/JovenesBundle/Form/InvitacionUsuarioType.php
@@ -24,7 +24,7 @@ class InvitacionUsuarioType extends AbstractType
         if ($evento->getPermitirObservaciones())
 	        $builder->add('observaciones',null,array('label' => 'Observaciones', 'required'=>false));
         if ($evento->getPermitirSuplente())
-	        $builder->add('suplente',null,array('label' => 'En caso de enviar un suplente (en vez de usted) indique su nombre por favor', 'required'=>false));
+	        $builder->add('suplente',null,array('label' => 'En caso de asistir un suplente en lugar de usted, indique su nombre y DNI por favor', 'required'=>false));
         
     }
 
