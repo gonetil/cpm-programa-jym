b960da3835e93347f20f43051553a1990b195155
gonetil@gmail.com <gonetil@gmail.com@6d4fc312-373e-aad0-5269-f45233a7996e>
2012-05-27 00:15:15 +0000

    las etapas tienen numero, y aparece la etapa de Rehacer proyecto

--- trunk/src/Cpm/JovenesBundle/StaticConfig.php
+++ trunk/src/Cpm/JovenesBundle/StaticConfig.php
@@ -7,11 +7,13 @@ class StaticConfig {
 	public static function getEtapas() {
 	$etapas = array ();
 	$etapas[]=array (
+	    'numero' => 1,
 	    'nombre' => 'Etapa 1 - Preparación',
 	    'accionesUsuario' => array (),
 	    'accionesProyecto' => array (),
 	);
 	$etapas[]=array (
+		'numero' => 2,
 	    'nombre' => 'Etapa 2 - Inscripción de propuestas',
 	    'accionesUsuario' => 
 	    array (
@@ -31,6 +33,7 @@ class StaticConfig {
 	    ),
 	  );
 	  $etapas[]=array (
+	    'numero' => 3,
 	    'nombre' => 'Etapa 3 - Desarrollo de proyectos',
 	    'accionesUsuario' => array (),
 	    'accionesProyecto' => array ( 
@@ -48,12 +51,32 @@ class StaticConfig {
 		    ), 
 	  );
 	  $etapas[]=array (
-	    'nombre' => 'Etapa 4 - Presentación de proyectos',
+	    'numero' => 4,
+	    'nombre' => 'Etapa 4 - Reenvio de proyectos rehacer',
+	    'accionesUsuario' => array (),
+	    'accionesProyecto' => array ( 
+		    'modificar_proyecto' => 
+		      array (
+		        'href' => 'proyecto_presentar',
+		        'label' => 'Enviar proyecto'
+		      ),
+		    'modificar_colaboradores' => 
+		      array (
+		        'href' => 'proyecto_edit_colaboradores',
+		        'label' => 'Agregar o elminar colaboradores'
+		      ) 
+		       
+		    ), 
+	  );
+	  $etapas[]=array (
+	    'numero' => 5,
+	    'nombre' => 'Etapa 5 - Presentación de proyectos',
 	    'accionesUsuario' => array (),
 	    'accionesProyecto' => array (),
 	  );
 	  $etapas[]=array (
-	    'nombre' => 'Etapa 5 - Etapa final',
+	    'numero' => 6,
+	    'nombre' => 'Etapa 6 - Etapa final',
 	    'accionesUsuario' => array (),
 	    'accionesProyecto' => array (),
 	  );
