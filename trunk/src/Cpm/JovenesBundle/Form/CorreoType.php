fd00ea9c06fb9040f5aa0549b331d254ce5a8f50
gonetil@gmail.com <gonetil@gmail.com@6d4fc312-373e-aad0-5269-f45233a7996e>
2013-02-20 21:29:17 +0000

    filtro proyectos de otros ciclos en la ventana de creación de correos

--- trunk/src/Cpm/JovenesBundle/Form/CorreoType.php
+++ trunk/src/Cpm/JovenesBundle/Form/CorreoType.php
@@ -14,7 +14,16 @@ class CorreoType extends AbstractType
             ->add('email', 'email')
             ->add('asunto')
             ->add('cuerpo')
-            ->add('proyecto', null, array('required'=>false, "empty_value"=>'Ninguno', 'label'=> "Sobre proyecto"))
+            ->add('proyecto', null, array('required'=>false, 
+										  "empty_value"=>'Ninguno', 
+											'label'=> "Sobre proyecto",
+											'query_builder' => function($er) {
+																        return $er->createQueryBuilder('p')
+																        	->innerJoin("p.ciclo","c")
+																        	->andWhere("c.activo = 1")
+																        	;
+																		}
+												))
         ;
     }
 
