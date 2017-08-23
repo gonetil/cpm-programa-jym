6f08c5ef2161dad2c6a20975dd2fef91a3ee4bc6
Gonzalo Villarreal <gonetil@gmail.com>
2017-08-23 19:58:41 -0300

    oculto el campo para solicitar archivos adjuntos al crear eventos. Con esto, dado que toma false por default, no deberia mostrarse nunca esta inforamcion

--- src/Cpm/JovenesBundle/Form/EventoType.php
+++ src/Cpm/JovenesBundle/Form/EventoType.php
@@ -24,8 +24,8 @@ class EventoType extends AbstractType
             ->add('numeroMaximoInvitados',null,array('label' => 'Limitar la lista de invitados a:', 'attr'=>array('class'=>'inline number'), 'required' => false,'data' => '0'))
             ->add('solicitarDuracionPresentacion','choice',array('label' => '¿Solicitar la duración (estimada) de la presentación?', 'expanded'=>true, 'choices' => array(1=>"si",0=>"no"), 'attr'=>array('class'=>'inline')))
             ->add('permitirModificarLaInvitacion','choice',array('label' => '¿Permitir modificar la invitación una vez aceptada o confirmada?', 'expanded'=>true, 'choices' => array(1=>"si",0=>"no"), 'attr'=>array('class'=>'inline')))
-            ->add('solicitarArchivoAdjunto','choice',array('label' => '¿Solicitar que se adjunte un archivo?', 'expanded'=>true, 'choices' => array(1=>"si",0=>"no"), 'attr'=>array('class'=>'inline')))
-            ->add('descripcionArchivoAdjunto',null,array('label'=>'Descripción del campo de archivo adjunto'))
+            ->add('solicitarArchivoAdjunto','choice',array('label' => '¿Solicitar que se adjunte un archivo?', 'expanded'=>true, 'choices' => array(1=>"si",0=>"no"), 'attr'=>array('class'=>'inline hidden')))
+            ->add('descripcionArchivoAdjunto',null,array('label'=>'Descripción del campo de archivo adjunto', 'attr'=>array('class'=>'hidden')))
             ->add('action','choice',array('label' => 'Acción a ejecutar una vez confirmada la invitación',
             	  'choices' => array('ConfirmacionCamposChapa'=>"Confirmación de datos del proyecto para Chapadmalal"),
 				  'expanded'=>false, 'empty_value' => "Ninguna",'attr'=>array('class'=>'inline'), 'required' => false))
